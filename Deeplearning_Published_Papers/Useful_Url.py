b9548609668a6652375f1645a6567cc46e090eb0
shankar88gt <shankar.88.gt@gmail.com>
2025-10-04 21:33:34 -0500

    Usefull urls

---
+++ Deeplearning_Published_Papers/Useful_Url.py
@@ -0,0 +1,35 @@
+""" 
+The Sequential model (TensorFlow Documentation)
+    https://www.tensorflow.org/guide/keras/sequential_model
+
+The Functional API
+    https://www.tensorflow.org/guide/keras/functional_api
+
+(He, Zhang, Ren & Sun, 2015)
+https://github.com/fchollet/deep-learning-models/blob/master/resnet50.py
+
+* YOLO: Real-Time Object Detection   (Redmon & Farhadi, 2016)
+1) https://pjreddie.com/darknet/yolo/ 
+2) https://github.com/allanzelener/YAD2K 
+
+(Taigman, Yang, Ranzato & Wolf)
+1) * face net   (GitHub: davidsandberg)
+ (Jason Brownlee, 2019)
+2) https://github.com/nyoki-mtl/keras-facenet/blob/master/notebook/tf_to_keras.ipynb
+
+* How to Develop a Face Recognition System Using FaceNet in Keras  (GitHub: davidsandberg)
+https://machinelearningmastery.com/how-to-develop-a-face-recognition-system-using-facenet-in-keras-and-an-svm-classifier/
+
+*  (Simonyan & Zisserman, 2015)
+* Pretrained models  (Jason Brownlee, 2019)
+https://www.vlfeat.org/matconvnet/pretrained/
+
+* Minimal character-level language model with a Vanilla Recurrent Neural Network, in Python/numpy  (GitHub: karpathy)
+https://gist.github.com/karpathy
+
+* The Unreasonable Effectiveness of Recurrent Neural Networks 
+* Man is to Computer Programmer as Woman is to Homemaker? Debiasing Word Embeddings   (Bolukbasi, Chang, Zou, Saligrama & Kalai, 2016)
+
+* GloVe: Global Vectors for Word Representation  
+https://nlp.stanford.edu/projects/glove/
+"""
